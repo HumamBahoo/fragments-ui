2c7ffd3a63aafaabcb8561b142334e3e5357f019
Humam Bahoo <humam.bahoo@gmail.com>
2023-04-13 19:58:03 -0400

    switched to react

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import "./App.css";
+
+function App() {
+  
+  return (
+    <div className="App">
+      <header>
+        <h1>Fragments UI</h1>
+        <nav>
+          <button>login</button>
+          <button>logout</button> 
+        </nav>
+      </header>
+    </div>
+  );
+}
+
+export default App;
